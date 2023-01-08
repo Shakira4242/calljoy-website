805ff2eeba04a50c7743c63be36a3335beac700c
Akash <akashshekara1212@gmail.com>
2023-01-08 15:18:43 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import Heading from './Heading.js';
+import NavigationBar from './NavigationBar.js';
+import Pricing from './Pricing.js';
+import FAQ from './FAQ.js';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="pricing/" element={<PricingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const HomePage = () => {
+  return (
+    <>
+      <NavigationBar/>
+      <Pricing/>
+    </>
+  );
+}
+
+const PricingPage = () => {
+  return (
+    <>
+      <NavigationBar/>
+      <Pricing/>
+    </>
   );
 }
 
