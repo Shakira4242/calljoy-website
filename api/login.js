df034993face287efef499e90828fc35cac427e2
Akash <akashshekara1212@gmail.com>
2023-01-15 17:03:20 -0600

    added login serverless stuff with supabase

---
+++ api/login.js
@@ -0,0 +1,35 @@
+// import supabase js
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  'https://bpeqsefkefhjnfshvrck.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwZXFzZWZrZWZoam5mc2h2cmNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE2NjAwNjAsImV4cCI6MTk4NzIzNjA2MH0.Wlq3HSKyUIRxSSv-Qm16L8prX8JrSe23NI7eYjACIAw'
+);
+
+// let phone = '+12107128563'
+
+// const { data, error } = await supabase.auth.signInWithOtp({ phone });
+
+// if(error) {
+//   console.log(error)
+// }else{
+//   console.log(data)
+// }
+
+// After receiving a SMS with a OTP.
+//
+// const { data, error } = await supabase.auth.verifyOtp({ phone: "+12107128563", token: "121338" })
+
+
+export default function handler(request, response) {
+  // get form data
+  const { phone } = request.body;
+
+  console.log(phone)
+
+  // response.status(200).json({
+  //   body: request.body,
+  //   query: request.query,
+  //   cookies: request.cookies,
+  // });
+}
--- src/Login.js
+++ src/Login.js
@@ -30,15 +30,15 @@ export default function Login(){
 		          <h1
 		            className="pt-10 mt-2 text-4xl font-bold text-gray-900 md:text-4xl"
 		          >
-		          	Get started with your phone number
+		          	Sign up to get your first 5 leads on us.
 		          </h1>
 	          </div>
 	        </div>
 
-	        <form action="/" className="mt-8 grid grid-cols-6 gap-6">
+	        <form action="/login" className="mt-8 grid grid-cols-6 gap-6">
 	          <div className="col-span-6 sm:col-span-3">
 	            <label
-	              for="FirstName"
+	              for="phone"
 	              className="block text-sm font-medium text-gray-700 text-gray-200"
 	            >
 	              Phone number
@@ -46,88 +46,28 @@ export default function Login(){
 
 	            <input
 	              type="tel"
-	              id="FirstName"
-	              name="first_name"
+	              id="phone"
+	              name="phone"
 	              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm border-gray-700 bg-gray-800 text-gray-200 font-bold"
 	            />
-	          </div>
-
-	          <div className="col-span-6 sm:col-span-3">
-	            <label
-	              for="Password"
-	              className="block text-sm font-medium text-gray-700 text-gray-200"
-	            >
-	              Password
-	            </label>
-
-	            <input
-	              type="password"
-	              id="Password"
-	              name="password"
-	              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm border-gray-700 bg-gray-800 text-gray-200"
-	            />
-	          </div>
-
-	          <div className="col-span-6 sm:col-span-3">
-	            <label
-	              for="PasswordConfirmation"
-	              className="block text-sm font-medium text-gray-700 text-gray-200"
-	            >
-	              Password Confirmation
-	            </label>
-
-	            <input
-	              type="password"
-	              id="PasswordConfirmation"
-	              name="password_confirmation"
-	              className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm border-gray-700 bg-gray-800 text-gray-200"
-	            />
-	          </div>
-
-	          <div className="col-span-6">
-	            <label for="MarketingAccept" className="flex gap-4">
-	              <input
-	                type="checkbox"
-	                id="MarketingAccept"
-	                name="marketing_accept"
-	                className="h-5 w-5 rounded-md border-gray-200 bg-white shadow-sm border-gray-700 bg-gray-800"
-	              />
-
-	              <span className="text-sm text-gray-700 text-gray-200">
-	                I want to receive emails about events, product updates and
-	                company announcements.
-	              </span>
-	            </label>
-	          </div>
-
-	          <div className="col-span-6">
-	            <p className="text-sm text-gray-500 text-gray-400">
-	              By creating an account, you agree to our
-	              <a href="#" className="text-gray-700 underline text-gray-200">
-	                terms and conditions
-	              </a>
-	              and
-	              <a href="#" className="text-gray-700 underline text-gray-200">
-	                privacy policy </a
-	              >.
-	            </p>
 	          </div>
 
 	          <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
 	            <button
 	              className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 hover:bg-blue-700 hover:text-white"
 	            >
-	              Create an account
+					Sign up
 	            </button>
 
-	            <p className="mt-4 text-sm text-gray-500 text-gray-400 sm:mt-0">
+	            <p className="mt-4 text-sm text-gray-900 font-semibold sm:mt-0">
 	              Already have an account?
-	              <a href="#" className="text-gray-700 underline text-gray-200"
+	              <a href="/login" className="text-gray-700 underline font-semibold text-gray-200"
 	                >Log in</a
 	              >.
 	            </p>
 	          </div>
 	        </form>
+			
 	      </div>
 	    </main>
 	  </div>
