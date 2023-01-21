3ea38208675a9112e7c9e07ebf56d499cff40321
Akash <akashshekara1212@gmail.com>
2023-01-21 09:54:02 -0600

    whatever

--- api/signup.js
+++ api/signup.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   'https://bpeqsefkefhjnfshvrck.supabase.co',
-  process.env.supabase_public_key
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwZXFzZWZrZWZoam5mc2h2cmNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE2NjAwNjAsImV4cCI6MTk4NzIzNjA2MH0.Wlq3HSKyUIRxSSv-Qm16L8prX8JrSe23NI7eYjACIAw'
 );
 
 // let phone = '+12107128563'
