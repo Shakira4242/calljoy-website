3f806ed4eb2ef28d12c2025a632a33be80af69f4
Akash <akashshekara1212@gmail.com>
2023-01-21 09:21:16 -0600

    added environment variable in auth.js

--- src/auth.js
+++ src/auth.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
     'https://bpeqsefkefhjnfshvrck.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwZXFzZWZrZWZoam5mc2h2cmNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE2NjAwNjAsImV4cCI6MTk4NzIzNjA2MH0.Wlq3HSKyUIRxSSv-Qm16L8prX8JrSe23NI7eYjACIAw'
+    process.env.SUPABASE_KEY
 )
 
 export default supabase
